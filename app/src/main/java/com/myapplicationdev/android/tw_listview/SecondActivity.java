1c90fd519f659a1c686bac0f83a4d85d5395adbd
Amal-Asyraf <41094396+Amal-Asyraf@users.noreply.github.com>
2019-04-30 14:08:00 +0900

    Amal

--- app/src/main/java/com/myapplicationdev/android/tw_listview/SecondActivity.java
+++ app/src/main/java/com/myapplicationdev/android/tw_listview/SecondActivity.java
@@ -3,13 +3,18 @@ package com.myapplicationdev.android.tw_listview;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class SecondActivity extends AppCompatActivity {
 
     ListView lv;
     TextView tvYear;
+    ArrayList<Module> modules;
+    ArrayAdapter aa;
 
 
     @Override
@@ -23,6 +28,35 @@ public class SecondActivity extends AppCompatActivity {
         Intent i = getIntent();
         String year = i.getStringExtra("year");
         tvYear.setText(year);
+
+
+
+        if(year.equalsIgnoreCase("year 1")){
+            Module module1 = new Module("C111",false);
+            Module module2 = new Module("C105", true);
+            Module module3 = new Module("A113",false);
+            modules.add(module1);
+            modules.add(module2);
+            modules.add(module3);
+        }else if(year.equalsIgnoreCase("year 2")){
+            Module module1 = new Module("C203",true);
+            Module module2 = new Module("C346", true);
+            Module module3 = new Module("C202",true);
+            modules.add(module1);
+            modules.add(module2);
+            modules.add(module3);
+        }else{
+            Module module1 = new Module("C347",true);
+            Module module2 = new Module("C349", true);
+            Module module3 = new Module("C302",true);
+            modules.add(module1);
+            modules.add(module2);
+            modules.add(module3);
+        }
+
+        aa = new ModuleAdapter(this, R.layout.row, modules);
+        lv.setAdapter(aa);
+
 
 
 
